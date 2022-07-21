72166456f94c78dee81f9bc0294ade39e1f8efce
zaidsidd360 <zaidsidd360@gmail.com>
2022-07-21 23:09:14 +0530

    pushing project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
+import { useState } from "react";
+
+const Modal = ({ setIsModalOpen, password, username }) => {
+  const [inputValue, setinputValue] = useState("");
+
+  const [isPassWordCorrect, setIsPasswordCorrect] = useState(true);
+
+  const handleSubmit = () => {
+    if (inputValue !== password) {
+      setIsPasswordCorrect(false);
+    } else {
+      setIsPasswordCorrect(true);
+      setIsModalOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <div className="modalback">
+        <div className="modal">
+          <p>password for 'https://{username}@github.com':</p>
+          <p className="graypass">
+            I knew you wouldn't remember it: {password}
+          </p>
+          <div>
+            <input
+              type="password"
+              name="gitpass"
+              onChange={(e) => {
+                setinputValue(e.target.value);
+              }}
+            />
+            <button
+              className="btn"
+              onClick={handleSubmit}
+              disabled={inputValue === "" ? "true" : ""}
+            >
+              OK
+            </button>
+          </div>
+          {!isPassWordCorrect ? (
+            <p className="redpass">Wrong password!</p>
+          ) : null}
+        </div>
+      </div>
+    </>
+  );
+};
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const appCallback = (homePushedValue) => {
+    setIsModalOpen(homePushedValue);
+  };
+
+  const username = "griffinStewie69";
+  const password = "loisMustDie";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isModalOpen ? (
+        <Modal
+          setIsModalOpen={setIsModalOpen}
+          password={password}
+          username={username}
+        />
+      ) : null}
+      <Home
+        appCallback={appCallback}
+        setIsModalOpen={setIsModalOpen}
+        password={password}
+        username={username}
+      />
     </div>
   );
 }
